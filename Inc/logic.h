f0df7b4feb57d29644004e55270f6926d37fcec6
James Gallegos <james.gallegos1618@gmail.com>
2025-09-26 13:17:55 -0700

    Update logic.h
    File used for trial #1 of maze

--- Inc/logic.h
+++ Inc/logic.h
@@ -7,17 +7,17 @@
 #define SENSOR_ERR       0xFFFFu
 
 // Motion thresholds and timings
-#define SAFE_DISTANCE_CM 15
+#define SAFE_DISTANCE_CM 7
 
 // Duration constants (ms)
-#define TURN_LEFT_90   180    // in-place 90° turn duration (tune this)
-#define TURN_RIGHT_90  100
-#define TURN_RIGHT_180 360
+#define TURN_LEFT_90   90    // in-place 90° turn duration (tune this)
+#define TURN_RIGHT_90  90
+#define TURN_RIGHT_180 180
 #define BACKUP         50
 #define IDLE           10
 
 // PWM duty cycle presets (%)
-#define TURN_DUTY        20
+#define TURN_DUTY        30
 #define CRUISE_DUTY      20
 #define BACKUP_DUTY      20
 
